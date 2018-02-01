41aad01789018b6a3bcff2c608192a1d2e39f827
huyup1e2n3g <35803175+huyup1e2n3g@users.noreply.github.com>
2018-02-01 14:43:33 +0800

    Create do_generator.py

---
+++ advance/do_generator.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s = (x * x for x in range(5))
+print(s)
+for x in s:
+    print(x)
+
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+f = fib(10)
+print('fib(10):', f)
+for x in f:
+    print(x)
+
+# call generator manually:
+g = fib(5)
+while 1:
+    try:
+        x = next(g)
+        print('g:', x)
+    except StopIteration as e:
+        print('Generator return value:', e.value)
+        break
+
+# 练习：杨辉三角定义如下：
+
+          1
+         / \
+        1   1
+       / \ / \
+      1   2   1
+     / \ / \ / \
+    1   3   3   1
+   / \ / \ / \ / \
+  1   4   6   4   1
+ / \ / \ / \ / \ / \
+1   5   10  10  5   1
+把每一行看做一个list，试写一个generator，不断输出下一行的list：
+
+# -*- coding: utf-8 -*-
+
+def triangles():
+    Y=[1]    
+    while True:
+        yield Y
+        Y=[1]+[Y[i]+Y[i+1] for i in range(len(Y)-1)]+[1]
+
+# 期待输出:
+# [1]
+# [1, 1]
+# [1, 2, 1]
+# [1, 3, 3, 1]
+# [1, 4, 6, 4, 1]
+# [1, 5, 10, 10, 5, 1]
+# [1, 6, 15, 20, 15, 6, 1]
+# [1, 7, 21, 35, 35, 21, 7, 1]
+# [1, 8, 28, 56, 70, 56, 28, 8, 1]
+# [1, 9, 36, 84, 126, 126, 84, 36, 9, 1]
+n = 0
+results = []
+for t in triangles():
+    print(t)
+    results.append(t)
